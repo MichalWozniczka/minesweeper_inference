4e68a93d2166a4b7a057f8301dca3bddd5f117a7
Michal Wozniczka <michalw@cs.utexas.edu>
2018-04-22 23:31:56 -0500

    need to figure out why sometimes chooses suboptimal move

--- minesweeper.py
+++ minesweeper.py
@@ -3,6 +3,7 @@ import ImageTk
 from PIL import Image
 import random
 import Queue
+import time
 
 class minesweeperGui():
     def __init__(self, w, h, ms):
@@ -45,17 +46,50 @@ class minesweeperGui():
 	self.canvas.create_image(self.offsx+50, 25, image=self.numbers[ms.numMines - (ms.numMines/10)*10])
 
     def draw(self, ms, displayBeliefs=True):
+        self.canvas.delete("all")
         for i in range(ms.height):
 	    for j in range(ms.width):
-	        if ms.flipped[i][j]:
+		if not ms.flipped[i][j] and ms.flags[i][j]:
+  		    self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.flag)
+	        elif ms.flipped[i][j]:
 		    if ms.grid[i][j] == -2:
      		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.explodedMine)
 		    elif ms.grid[i][j] == -1:
      		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.flag)
 	            else:
      		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.opened[ms.grid[i][j]])
+		elif ms.beliefs[i][j] == 0:
+  		    self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.flag)
 		elif displayBeliefs:
-  		    self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.beliefs[int(ms.beliefs[i][j]*20)])
+  		    self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.beliefs[int((ms.antibeliefs[i][j])*19)])
+		else:
+    		    self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.unopened)
+
+	self.canvas.create_image(self.offsx+10, 25, image=self.numbers[ms.numMines/100])
+	self.canvas.create_image(self.offsx+30, 25, image=self.numbers[(ms.numMines - (ms.numMines/100)*100)/10])
+	self.canvas.create_image(self.offsx+50, 25, image=self.numbers[ms.numMines - (ms.numMines/10)*10])
+
+    def lossScreen(self, ms, displayBeliefs=True):
+        self.canvas.delete("all")
+        for i in range(ms.height):
+	    for j in range(ms.width):
+		if not ms.flipped[i][j] and ms.flags[i][j]:
+		    if ms.grid[i][j] == -2:
+  		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.flag)
+		    else:
+  		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.wrongMine)
+	        elif ms.grid[i][j] == -2:
+                    if ms.flipped[i][j]:
+     		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.explodedMine)
+	            else:
+     		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.mine)
+	        elif ms.flipped[i][j]:
+		    if ms.grid[i][j] == -1:
+     		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.flag)
+	            else:
+     		        self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.opened[ms.grid[i][j]])
+		elif displayBeliefs:
+  		    self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.beliefs[int((ms.antibeliefs[i][j])*19)])
 		else:
     		    self.canvas.create_image(j*self.tileSize+self.offsx, i*self.tileSize+self.offsy, image=self.unopened)
 
@@ -68,7 +102,9 @@ class minesweeper():
     def __init__(self, w, h, numMines):
         self.grid = [[0 for i in range(w)] for j in range(h)]
 	self.flipped = [[False for i in range(w)] for j in range(h)]
-	self.beliefs = [[float(numMines)/(w*h) for i in range(w)] for j in range(h)]
+	self.beliefs = [[1 - float(numMines)/(w*h) for i in range(w)] for j in range(h)]
+	self.antibeliefs = [[float(numMines)/(w*h) for i in range(w)] for j in range(h)]
+	self.flags = [[False for i in range(w)] for j in range(h)]
 	self.width = w
 	self.height = h
 	self.numMines = numMines
@@ -100,23 +136,148 @@ class minesweeper():
 	    if curpos in visited:
 	      continue
 	    visited.add(curpos)
+	    self.flipped[curpos[0]][curpos[1]] = True
             for a in range(-1, 2):
 	        for b in range(-1, 2):
-		    if curpos[0]+a < 0 or curpos[0]+a >= self.height or curpos[1]+b < 0 or curpos[1]+b >= self.height:
+		    if curpos[0]+a < 0 or curpos[0]+a >= self.height or curpos[1]+b < 0 or curpos[1]+b >= self.height or self.grid[curpos[0]][curpos[1]] != 0:
 		        continue
 		    self.flipped[curpos[0]+a][curpos[1]+b] = True
 		    if self.grid[curpos[0]+a][curpos[1]+b] == 0:
                	        queue.put((curpos[0]+a, curpos[1]+b))
 
-def main():  
-    ms = minesweeper(30, 30, 50)
-    gui = minesweeperGui(500, 500, ms)
-    ms.flip(0, 0)
+    def unflippedTiles(self):
+        cnt = 0
+	for i in range(self.height):
+	    for j in range(self.width):
+	        if self.flipped[i][j] == False:
+		    cnt += 1
 
-    while(1):
-        #gui.displayBeliefs(ms)
-	gui.draw(ms)
+	return cnt
+
+    def actOnNeighbors(self, x, y):
+        unflippedNeighborCount = 0
+	flagCount = 0
+	neighbors = []
+
+        for a in range(-1, 2):
+	    for b in range(-1, 2):
+	        if x+a < 0 or x+a >= self.height or y+b < 0 or y+b >= self.width:
+		    continue
+		if not self.flipped[x+a][y+b]:
+		    unflippedNeighborCount += 1
+		    if self.flags[x+a][y+b] or self.beliefs[x+a][y+b] == 0:
+		        flagCount += 1
+		    neighbors.append((x+a, y+b))
+
+        for a in range(-1, 2):
+	    for b in range(-1, 2):
+	        if x+a < 0 or x+a >= self.height or y+b < 0 or y+b >= self.width:
+		    continue
+		if not self.flipped[x+a][y+b] and unflippedNeighborCount != flagCount:
+		    self.beliefs[x+a][y+b] *= 1.0 - ((self.grid[x][y] - flagCount) / (float(unflippedNeighborCount) - flagCount))
+		    if not self.flags[x+a][y+b] and self.beliefs[x+a][y+b] > 0:
+		        self.antibeliefs[x+a][y+b] *= ((self.grid[x][y] - flagCount) / (float(unflippedNeighborCount) - flagCount))
+
+        unflippedTiles = self.unflippedTiles()
+        for i in range(self.height):
+	    for j in range(self.width):
+	        if not self.flipped[i][j] and (i, j) not in neighbors:
+		    self.beliefs[i][j] *= 1.0 - (float(self.numMines - self.grid[x][y]) / (unflippedTiles - unflippedNeighborCount))
+		    if not self.flags[i][j] and self.beliefs[i][j] > 0:
+		        self.antibeliefs[i][j] *= (float(self.numMines - self.grid[x][y]) / (unflippedTiles - unflippedNeighborCount))
+
+    def updateBeliefs(self):
+	self.beliefs = [[1 - float(self.numMines)/(self.width*self.height) for i in range(self.width)] for j in range(self.height)]
+	self.antibeliefs = [[float(self.numMines)/(self.width*self.height) for i in range(self.width)] for j in range(self.height)]
+        for i in range(self.height):
+	    for j in range(self.width):
+	        if self.flipped[i][j]:
+		    self.beliefs[i][j] = 0
+		    self.antibeliefs[i][j] = 1
+		    if self.grid[i][j] > 0:
+		        self.actOnNeighbors(i, j)
+
+	for i in range(self.height):
+	    for j in range(self.width):
+	        if self.beliefs[i][j] == 0:
+		    self.flags[i][j] = True
+
+    def normalizeBeliefs(self):
+        sumBeliefs = 0
+        sumAntiBeliefs = 0
+	unflippedTiles = self.unflippedTiles()
+
+        maxBeliefs = 0
+	maxAntiBeliefs = 0
+        for i in range(self.height):
+	    for j in range(self.width):
+	        if not self.flipped[i][j]:
+	            maxBeliefs = max(maxBeliefs, self.beliefs[i][j])
+		    sumBeliefs += self.beliefs[i][j]
+		    if not self.flags[i][j]:
+		        sumAntiBeliefs += self.antibeliefs[i][j]
+	                maxAntiBeliefs = max(maxAntiBeliefs, self.antibeliefs[i][j])
+
+	for i in range(self.height):
+	    for j in range(self.width):
+	        #self.beliefs[i][j] *= (unflippedTiles - self.numMines) / sumBeliefs
+		if not self.flipped[i][j]:
+		    if sumBeliefs > 0:
+		        self.beliefs[i][j] /= maxBeliefs
+		    if sumAntiBeliefs > 0:
+		        self.antibeliefs[i][j] /= maxAntiBeliefs
+
+    def actOnBeliefs(self):
+        maxval = 0
+	argmax = (0, 0)
+
+	for i in range(self.height):
+	    for j in range(self.width):
+	        belief = 1.0 - self.antibeliefs[i][j]
+	        if belief >= maxval and not self.flags[i][j] and not self.flipped[i][j]:
+		    maxval = belief
+		    argmax = (i, j)
+
+	self.flip(argmax[0], argmax[1])
+	return self.grid[argmax[0]][argmax[1]] == -2
+
+    def loss(self):
+        for i in range(self.height):
+	    for j in range(self.width):
+	        if self.flipped[i][j] and self.grid[i][j] == -2:
+		    return True
+        return False
+
+    def finished(self):
+        sumBeliefs = sum(sum(self.beliefs, []))
+        return sumBeliefs == 0 or self.loss()
+
+def main():  
+    ms = minesweeper(22, 22, 100)
+    gui = minesweeperGui(800, 800, ms)
+    while True:
+    #ms.grid = [[-2, 0, -2], [1, 2, 1], [0,0,0]]
+    #ms.flipped = [[False, False, False], [True, True, True], [True,True,True]]
+    #ms.beliefs = [[1.0/3, 1.0/3, 1.0/3], [0, 0, 0], [0,0,0]]
+
+        while not ms.finished():
+            gui.draw(ms)
+            gui.window.update()
+	    if ms.actOnBeliefs():
+	        break
+            ms.updateBeliefs()
+	    ms.normalizeBeliefs()
+
+        gui.draw(ms)
+
+        
+        if ms.loss():
+	    gui.lossScreen(ms)
         gui.window.update()
+
+	raw_input("Press enter to continue")
+
+        ms = minesweeper(22, 22, 100)
 
 if __name__ == "__main__": main()
     
